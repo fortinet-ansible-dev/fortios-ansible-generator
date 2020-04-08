7d59f0fb24765f7341d718ac8582fa82346e9e7d
Link Zheng <zhengl@fortinet.com>
2020-04-08 09:44:36 +0000

    Part II: compledted document generation

--- generate_doc.py
+++ generate_doc.py
@@ -31,29 +31,54 @@ def generate_parameters(params, layer):
         key_type = param['type']
         if key_type in ['str', 'bool', 'int'] or (key_type == 'list' and 'choices' in param):
             ddata += ' ' * layer * 4 + '<li>'
-            ddata += '<span class="li-head">' + key + '</span>'
+            ddata += ' <span class="li-head">' + key + '</span>'
             ddata += ' - %s' % (key_desc)
-            ddata += '<span class="li-normal">type: %s</span>' % (key_type)
+            ddata += ' <span class="li-normal">type: %s</span>' % (key_type)
             if 'required' in param:
-                ddata += '<span class="li-required">required: %s</span>' % (param['required'])
+                ddata += ' <span class="li-required">required: %s</span>' % (param['required'])
             if 'default' in param:
-                ddata += '<span class="li-normal">default: %s</span>' % (param['default'])
+                ddata += ' <span class="li-normal">default: %s</span>' % (param['default'])
             if 'choices' in param:
-                ddata += '<span class="li-normal">choices: %s</span>' % (array_to_string(param['choices']))
+                ddata += ' <span class="li-normal">choices: %s</span>' % (array_to_string(param['choices']))
             ddata += '</li>\n'
         elif key_type == 'dict' or (key_type == 'list' and 'suboptions' in param):
             ddata += ' ' * layer * 4 + '<li>'
-            ddata += '<span class="li-head">' + key + '</span>'
+            ddata += ' <span class="li-head">' + key + '</span>'
             ddata += ' - %s' % (key_desc)
-            ddata += '<span class="li-normal">type: %s</span>' % (key_type)
+            ddata += ' <span class="li-normal">type: %s</span>' % (key_type)
             ddata += '</li>\n'
             ddata += ' ' * (layer + 1) * 4 + '<ul class="ul-self">\n'
             assert('suboptions' in param)
             ddata += generate_parameters(param['suboptions'], layer + 1)
             ddata += ' ' * (layer + 1) * 4 + '</ul>\n'
         else:
-            assert(False)
+            
+            print('incomplete schema: key:%s key_type:%s' % (key, key_type))
+           #assert(False)
     return ddata
+
+def format_example(example):
+    ddata = ''
+    for line in example.splitlines():
+        ddata += '    ' + line + '\n'
+    return ddata
+def generate_return(doc_ret):
+    ddata = ''
+    for key in doc_ret:
+        item = doc_ret[key]
+        key_type = item['type']
+        key_desc = item['description'] if 'description' in item else 'No decription'
+        ddata += '    <li>'
+        ddata += ' <span class="li-return">%s</span>' % (key)
+        ddata += ' - %s' % (key_desc)
+        if 'returned' in item:
+            ddata += ' <span class="li-normal">returned: %s</span>' % (item['returned'])
+        ddata += ' <span class="li-normal">type: %s</span>' % (key_type)
+        if 'sample' in item:
+            ddata += ' <span class="li-normal">sample: %s</span>' % (item['sample'])
+        ddata += '</li>\n'
+    return ddata
+
 def generate_document(mod, dst):
     ddata = ''
     mod_doc = yaml.load(mod.DOCUMENTATION)
@@ -96,9 +121,46 @@ def generate_document(mod, dst):
     ddata += generate_parameters(mod_doc['options'], 1)
     ddata += '    </ul>\n\n\n'
 
+    ddata += 'Notes\n'
+    ddata += '-----\n\n'
+    ddata += '.. note::\n\n'
+    for item in mod_doc['notes']:
+        ddata += '   - %s\n\n' % (item)
+    ddata += '\n\n'
+   
+    ddata += 'Examples\n'
+    ddata += '--------\n\n'
+    ddata += '.. code-block:: yaml+jinja\n'
+    ddata += format_example(mod.EXAMPLES)
 
-    print(json.dumps(mod_doc))
-    print(ddata)
+    ddata += '\n\n'
+    ddata += 'Return Values\n'
+    ddata += '-------------\n'
+    ddata += 'Common return values are documented: https://docs.ansible.com/ansible/latest/reference_appendices/common_return_values.html#common-return-values, the following are the fields unique to this module:\n\n'
+    ddata += '.. raw:: html\n\n'
+
+    mod_ret = yaml.load(mod.RETURN)
+    ddata += '    ' + '<ul>\n\n'
+    ddata += generate_return(mod_ret)
+    ddata += '    ' + '</ul>\n\n'
+
+    ddata += 'Status\n'
+    ddata += '------\n\n'
+    ddata += '- This module is not guaranteed to have a backwards compatible interface.\n\n\n'
+
+    ddata += 'Authors\n'
+    ddata += '-------\n\n'
+
+    for item in mod_doc['author']:
+        ddata += '- %s\n' % (item)
+    ddata += '\n\n'
+
+    ddata += '.. hint::\n'
+    ddata += '    If you notice any issues in this documentation, you can create a pull request to improve it.\n'
+
+    with open(dst, 'w') as f:
+        f.write(ddata)
+
 
 def main():
     if len(sys.argv) != 3:
