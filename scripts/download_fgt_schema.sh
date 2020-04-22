746bcf704a2ca9acaef580a2f9a27310ca7e60f1
Link Zheng <zhengl@fortinet.com>
2020-04-22 02:50:08 +0000

    minor change for downloading schema file

--- scripts/download_fgt_schema.sh
+++ scripts/download_fgt_schema.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
 
-echo -e "\e[34mEnter the fortigate address (e.g.: 192.168.102.32):\e[0m"
-read FGT_ADDRESS
-echo -e "\e[34mEnter the username:\e[0m"
-read USER_NAME
-echo -e "\e[34mEnter the password:\e[0m"
-read PASSWORD
+. ./scripts/common
 
 SHOULD_DOWNLOAD=yes
 SCHEMA_PATH=./fgt_schema.json
 
 if test -f $SCHEMA_PATH; then
-    version=`cat fgt_schema.json |grep '^  \"version\"'`
-    echo "Existing file found with version:", $version
+    version=$(get_schema_version)
+    echo -e "Existing schema file found with version:$BGre$version$RCol"
     echo -n -e "\e[34mReplace the existing file or not [yes/no]?\e[0m"
     read SHOULD_DOWNLOAD
 fi
@@ -21,6 +16,14 @@ if [ "${SHOULD_DOWNLOAD}" != "yes" ]; then
     echo "Using the existing file."
     exit 0
 fi
+
+echo -n -e "\e[34mEnter the fortigate address (e.g.: 192.168.102.32):\e[0m"
+read FGT_ADDRESS
+echo -n -e "\e[34mEnter the username:\e[0m"
+read USER_NAME
+echo -n -e "\e[34mEnter the password:\e[0m"
+read -s PASSWORD
+echo ''
 
 curl -k -i -X POST https://${FGT_ADDRESS}/logincheck -d "username=${USER_NAME}&secretkey=${PASSWORD}" --dump-header /tmp/headers.txt
 
