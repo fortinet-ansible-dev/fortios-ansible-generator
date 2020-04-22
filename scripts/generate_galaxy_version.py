067c55eab4fcb110c642312724fbd9a5bc9b93f9
Link Zheng <zhengl@fortinet.com>
2020-04-22 09:05:52 +0000

    toolchain fully automated

---
+++ scripts/generate_galaxy_version.py
@@ -0,0 +1,25 @@
+#! /usr/bin/python3
+import json
+import sys
+
+def main():
+    target_version = sys.argv[1]
+    versions = None
+    with open('galaxy_version.yml') as f:
+        versions = json.loads(f.read())
+    fos_versions = list(versions.keys())
+    fos_versions.sort()
+
+    print('| FOS version|Galaxy  Version| Release date|Install Path |')
+    print('|----------|:-------------:|:-------------:|:------:|')
+
+    for fos in fos_versions:
+        galaxy_versions = list(versions[fos].keys())
+        galaxy_versions.sort()
+        for galaxy in galaxy_versions:
+            if galaxy > target_version:
+                continue
+            date = versions[fos][galaxy]
+            print('|%s|%s %s|%s|`ansible-galaxy collection install fortinet.fortios:%s`|' % (fos, galaxy, '`latest`' if galaxy == galaxy_versions[-1] else '', date, galaxy))
+if __name__ == '__main__':
+    main()
